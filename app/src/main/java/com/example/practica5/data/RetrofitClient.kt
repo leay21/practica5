7f042260798c4246d9ab96fcd3ebe25a2b8ac48a
leay21 <yaelt143@gmail.com>
2026-01-06 18:37:04 -0600

    Versión de la app sin usuario

--- app/src/main/java/com/example/practica5/data/RetrofitClient.kt
+++ app/src/main/java/com/example/practica5/data/RetrofitClient.kt
@@ -1,8 +1,7 @@
 package com.example.practica5.data
 
-import com.example.practica5.model.FavoriteRequest
-import com.example.practica5.model.FavoriteResponse
-import com.example.practica5.model.TvMazeResponse
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
@@ -10,14 +9,15 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
+import com.example.practica5.model.FavoriteRequest // Asegura que estos imports sean correctos
+import com.example.practica5.model.FavoriteResponse
+import com.example.practica5.model.TvMazeResponse
 
-// 1. Interfaz para TVMaze
 interface TvMazeApi {
     @GET("search/shows")
     suspend fun searchShows(@Query("q") query: String): List<TvMazeResponse>
 }
 
-// 2. Interfaz para TU Backend (Node.js)
 interface MyBackendApi {
     @POST("api/favoritos")
     suspend fun syncFavorite(@Body fav: FavoriteRequest): retrofit2.Response<Void>
@@ -26,23 +26,32 @@ interface MyBackendApi {
     suspend fun getRemoteFavorites(@Path("userId") userId: Int): List<FavoriteResponse>
 }
 
-// 3. Objeto Singleton para crear las conexiones
 object RetrofitClient {
 
-    // Cliente para TVMaze
+    // 1. CREAMOS EL "ESPÍA" (INTERCEPTOR)
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY // Veremos todo: Cabeceras y Datos
+    }
+
+    // 2. CREAMOS UN CLIENTE HTTP QUE USE ESE ESPÍA
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
     val tvMazeApi: TvMazeApi by lazy {
         Retrofit.Builder()
             .baseUrl("https://api.tvmaze.com/")
+            .client(client) // <-- Le enchufamos el cliente con logs
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(TvMazeApi::class.java)
     }
 
-    // Cliente para TU servidor Local
-    // IMPORTANTE: En el emulador Android, "localhost" es "10.0.2.2"
     val myApi: MyBackendApi by lazy {
         Retrofit.Builder()
+            // REVISA: Asegúrate que esta IP sea la de tu PC (ipconfig)
             .baseUrl("http://192.168.100.147:3000/")
+            .client(client) // <-- Le enchufamos el cliente con logs aquí también
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(MyBackendApi::class.java)
